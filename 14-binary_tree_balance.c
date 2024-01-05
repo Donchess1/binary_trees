5281bccfbe1d4aefe5d806db5d752abcb973342a
Donchess1 <Stepabod@yahoo.com>
2024-01-05 07:17:08 +0100

    Update 14-binary_tree_balance.c

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -24,7 +24,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	if (tree)
 	{
-		size_t  a= 0, c = 0;
+		size_t  a = 0, c = 0;
 
 		a = tree->left ? 1 + binary_tree_height(tree->left) : 1;
 		c = tree->right ? 1 + binary_tree_height(tree->right) : 1;
